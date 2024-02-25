b8538f9bd2acd59fbfae725e6bb844ee8ed8d660
Taha Ansari <taha.m.ansari@gmail.com>
2024-02-25 01:56:29 -0800

    inint

---
+++ app/page.js
@@ -0,0 +1,13 @@
+import Link from "next/link"
+
+const Homepage = () => {
+  return (
+    <div>
+      <h1>Homepage</h1>
+      <Link href="/about">About</Link>
+    </div>
+
+  )
+}
+
+export default Homepage
